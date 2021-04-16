51d04d402f2d1941cc6a25d044caade7e9d307c0
dstearle <dallas.st.earle@gmail.com>
2021-04-16 12:34:47 -0700

    Set the button for showing newer items in the list to hide when at the beginning of the list

--- src/components/ShowsList.vue
+++ src/components/ShowsList.vue
@@ -12,7 +12,11 @@
         <div class="inline-flex">
 
             <!-- Newer Button -->
-            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" @click="newer()">
+            <button 
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" 
+                @click="newer()"
+                v-show="this.currentIndex > 4"
+            >
                 Newer
             </button>
 
