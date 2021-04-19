d23e11b12674d03783ab4f51e8cbe526984396a2
dstearle <dallas.st.earle@gmail.com>
2021-04-19 13:47:51 -0700

    Setup the app to use the data from the firestore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/tailwind.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDqvDBZhWIC4mPRjo0soLd-7EbWhHnBTj0",
+    authDomain: "lbs-main-floor-calendar.firebaseapp.com",
+    projectId: "lbs-main-floor-calendar",
+    storageBucket: "lbs-main-floor-calendar.appspot.com",
+    messagingSenderId: "74944315304",
+    appId: "1:74944315304:web:6b0af48a52a74f9a14679d"
+});
+
+export const db = firebase.firestore();
 
 createApp(App).mount('#app')
